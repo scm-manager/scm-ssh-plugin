26f0d6d4cd064efd678afe1a812dc21e1bd11abb
René Pfeuffer <rene.pfeuffer@clouddogu.com>
2019-03-04 14:26:57 +0100

    Move classes to scm core

--- src/main/java/com/cloudogu/scm/ssh/command/git/GitSshModule.java
+++ src/main/java/com/cloudogu/scm/ssh/command/git/GitSshModule.java
@@ -2,14 +2,10 @@ package com.cloudogu.scm.ssh.command.git;
 
 import com.google.inject.AbstractModule;
 import sonia.scm.protocolcommand.CommandParser;
-import sonia.scm.protocolcommand.RepositoryContextResolver;
-import sonia.scm.protocolcommand.ScmSshProtocol;
 
 public class GitSshModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(CommandParser.class).to(GitCommandParser.class);
-    bind(RepositoryContextResolver.class).to(GitRepositoryContextResolver.class);
-    bind(ScmSshProtocol.class).to(GitSshProtocol.class);
   }
 }
