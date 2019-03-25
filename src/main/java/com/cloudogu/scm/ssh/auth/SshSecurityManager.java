faeebef79f293edd954c78166b78b4a5efadc887
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-03-25 11:06:24 +0100

    suppress maximum inheritance depth warning of SonarQube

--- src/main/java/com/cloudogu/scm/ssh/auth/SshSecurityManager.java
+++ src/main/java/com/cloudogu/scm/ssh/auth/SshSecurityManager.java
@@ -10,6 +10,7 @@ import javax.inject.Inject;
  * SecurityManager used for ssh authentication and authorization. The {@link SshSecurityManager} should be injected as
  * a {@link javax.inject.Provider} to avoid circular dependency resolution problems.
  */
+@SuppressWarnings("squid:MaximumInheritanceDepth")
 public class SshSecurityManager extends DefaultSecurityManager {
 
   @Inject
